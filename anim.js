1114ac66799d8f10e10221206568da3bc3d7e032
dfree <d@dfree.co.uk>
2018-10-26 15:22:06 +0200

    calculator part

--- anim.js
+++ anim.js
@@ -1,9 +1,111 @@
 (function ($) {
 	window.onload = function(e) {
 		var init, W, H;
-
 		var W = 738;
-		var H = 300;
+		var H = 500;
+
+		var printer = [
+			{
+				output:"Number of drivers trained: ",
+				calc: function(n, _ranges, _data){return n}
+			},
+			{
+				output:"Monthly subscription: ",
+				calc: function(n, _ranges, _data){
+					var r = -1;
+					var c = 0;
+					while(r < 0 && c < _ranges.length){
+						if(n >= _ranges[c][0] && n <= _ranges[c][1]){
+							r = c;
+						}
+						c++;
+					}
+					return "£"+(Math.round((_ranges[r][1]*_data[r]/12)*10)/10);
+				}
+			},
+			{
+				output:"Yearly subscription: ",
+				calc: function(n, _ranges, _data){
+					var r = -1;
+					var c = 0;
+					while(r < 0 && c < _ranges.length){
+						if(n >= _ranges[c][0] && n <= _ranges[c][1]){
+							r = c;
+						}
+						c++;
+					}
+					return "£"+(_ranges[r][1]*_data[r]);
+				}
+			},
+			{
+				output:"Savings over SUD: ",
+				calc: function(n, _ranges, _data){
+					var r = -1;
+					var c = 0;
+					while(r < 0 && c < _ranges.length){
+						if(n >= _ranges[c][0] && n <= _ranges[c][1]){
+							r = c;
+						}
+						c++;
+					}
+					return "£"+(150*_ranges[r][1]-_ranges[r][1]*_data[r]);
+				}
+			}
+
+		];
+		var ranges = [
+			[6,10],
+			[11,25],
+			[26,50],
+			[51,100],
+			[101,200],
+			[201,1000],
+			[1001,5000]
+		];
+
+		var data = {
+			workshop:{
+				basic:[	
+					45.5,
+					43,
+					41,
+					39.5,
+					37,
+					35,
+					33
+				],
+				pro:[
+					57.5,
+					57,
+					55,
+					52.5,
+					47.5,
+					42.5,
+					41.5
+				]
+			},
+			licence:{
+				basic:[	
+					30,
+					30,
+					27.5,
+					25,
+					22.5,
+					20,
+					17.5
+				],
+				pro:[
+					37.5,
+					35,
+					32.5,
+					30,
+					27.5,
+					25,
+					22.5
+				]
+			}
+
+		}
 		var gap = 10;
 		var passive_y = 80;
 		var element_height = 60;
@@ -14,7 +116,6 @@
 			"calc_sub_buttons",
 			"calc_fors",
 			"calc_input"
-			
 		];
 		var elements_data = {};
 		var state = 0;
@@ -31,7 +132,25 @@
 			//$.id("wrapper").innerHTML = "Hello";
 		}();
 
+		function handleSlider(e){
+			var val = $.id("calc_slider").value;
+			$.id("calc_drivers_num").innerHTML = val + (val == 1001 ? "+" : "");
+		}
 		function reset() {
+			$.set("calc_slider", {x:gap, y:320, width:W-4*gap});
+			$.id("calc_slider").addEventListener("input", handleSlider);
+			$.set("calc_drivers_num", {y:40, color:"#777777"});
+			$.set("calc_cover", {autoAlpha:0});
+			$.id("calc_close").addEventListener("click", closeParser);
+			$.set("calc_input_submit", {
+				x:gap+small_setup.width+gap,
+				y:345,
+				width:small_setup.width,
+				height:small_setup.height,
+				borderRadius:"25px 25px 25px 25px"
+			})
+			$.id("calc_input_submit").addEventListener("click", parser);
+
 			for(var i = 0; i < elements.length; i++){
 				var full_width = W-2*gap;
 				$.set(elements[i], {
@@ -172,6 +291,20 @@
 				$.tween("calc_title", 0.4, {autoAlpha:0, ease:Power2.easeOut});
 			}
 		};
+		function parser(){
+			var html = "";
+			var driver_num = parseInt($.id("calc_drivers_num").innerHTML);
+			var _data = data[elements_data["calc_main_buttons"] == 0 ? "workshop" : "licence"][elements_data["calc_sub_buttons"] == 0 ? "basic" : "pro"];
+			for(var i = 0; i < printer.length; i++){
+				html += "<div>"+printer[i].output +"<span>"+printer[i].calc(driver_num, ranges, _data)+"</span></div>";
+			}
+			$.id("calc_cover_output").innerHTML = html;
+			$.tween("calc_cover", 0.8, {autoAlpha:1, ease:Power1.easeInOut});
+			console.log(html);
+		}
+		function closeParser(){
+			$.tween("calc_cover", 0.6, {autoAlpha:0, ease:Power1.easeInOut});
+		}
 	}
 })({
 	id: function(name){
