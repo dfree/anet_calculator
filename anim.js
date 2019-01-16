e5da18b2cfbffbb9181a69bc467cf7d213f7e1fa
David Szucs <d@dfree.co.uk>
2019-01-16 21:39:07 +0100

    update

--- anim.js
+++ anim.js
@@ -4,7 +4,7 @@
 		var W = 738;
 		var H = 500;
 
-		var printer = [
+		/*var printer = [
 			{
 				output:"Number of drivers trained: ",
 				calc: function(n, _ranges, _data){return n}
@@ -31,7 +31,7 @@
 				}
 			}
 
-		];
+		];*/
 		
 		var ranges = [
 			[1,10],
@@ -44,47 +44,24 @@
 		];
 
 		var data = {
-			workshop:{
-				basic:[	
-					45.5,
-					43,
-					41,
-					39.5,
-					37,
-					35,
-					33
-				],
-				pro:[
-					57.5,
-					57,
-					55,
-					52.5,
-					47.5,
-					42.5,
-					41.5
-				]
-			},
-			licence:{
-				basic:[	
-					30,
-					30,
-					27.5,
-					25,
-					22.5,
-					20,
-					17.5
-				],
-				pro:[
-					37.5,
-					35,
-					32.5,
-					30,
-					27.5,
-					25,
-					22.5
-				]
-			}
-
+			basic:[	
+				40,
+				39.5,
+				37,
+				35,
+				33,
+				31,
+				29
+			],
+			pro:[
+				50,
+				47,
+				45.5,
+				43,
+				41,
+				39.5,
+				37
+			]
 		}
 		var gap = 10;
 		var passive_y = 80;
@@ -92,9 +69,6 @@
 		var details_height = 300;
 		var details_top = 80;
 		var elements = [
-			"calc_main_buttons",
-			"calc_sub_buttons",
-			"calc_fors",
 			"calc_input"
 		];
 		var elements_data = {};
@@ -105,192 +79,55 @@
 			height: 36,
 		}
 
-		var people_length = 7;
-		var people_state = [];
-		var people_gap = 40;
 
 		init = function() {
 			TweenPlugin.activate([CSSPlugin]);
 			$.id("wrapper").style.display = "block";
 			reset();
-			processPeople();
-			loadNew(0);
+			parser(1)
+			//loadNew(0);
 			//$.id("wrapper").innerHTML = "Hello";
 		}();
 
 		function handleSlider(e){
-			var max = 1000;
-			var val = $.id("calc_slider").value;
-			var s_val = getSectionValue(val, ranges, max);
-			checkPeople(val, ranges, max);
-			$.id("calc_drivers_num").innerHTML = (val == max ? "5000+" : s_val);
-			
+			parser($.id("calc_slider").value);
 		}
 		function reset() {
-			$.set("calc_slider", {x:gap, y:320, width:W-4*gap});
 			$.id("calc_slider").addEventListener("input", handleSlider);
-			$.set("calc_drivers_num", {y:263, color:"#777777"});
-			$.set("calc_cover", {autoAlpha:0});
-			$.id("calc_close").addEventListener("click", closeParser);
-			$.set("calc_input_submit", {
-				x:gap+small_setup.width+gap,
-				y:345,
-				width:small_setup.width,
-				height:small_setup.height,
-				borderRadius:"25px 25px 25px 25px"
-			})
-			$.id("calc_input_submit").addEventListener("click", parser);
-
-			for(var i = 0; i < elements.length; i++){
-				var full_width = W-2*gap;
-				$.set(elements[i], {
-					width:full_width,
-					height:0,
-					x:gap,
-					y:passive_y,
-					autoAlpha: 0
-				});
-				if(elements[i] != "calc_input"){
-					$.id(elements[i]+"_0").classList.add("button");
-					$.id(elements[i]+"_0").addEventListener("click", clicked);
-					$.set(elements[i]+"_0", {
-						x:0,
-						y:0,
-						width:full_width/2,
-						height:element_height,
-						borderRadius:"25px 0px 0px 25px"
-					});
-					$.id(elements[i]+"_1").classList.add("button");
-					$.id(elements[i]+"_1").addEventListener("click", clicked);
-					$.set(elements[i]+"_1", {
-						x:full_width/2,
-						y:0,
-						width:full_width/2,
-						height:element_height,
-						borderRadius:"0px 25px 25px 0px"
-					});
-					if($.id(elements[i]+"_0_details")){
-						$.id(elements[i]+"_0_details").classList.add("details");
-						$.set(elements[i]+"_0_details", {
-							x:0,
-							y:details_top,
-							width:full_width/2,
-							height:details_height,
-							borderRadius:"25px 0px 0px 25px"
-						});
-					}
-					if($.id(elements[i]+"_1_details")){
-						$.id(elements[i]+"_1_details").classList.add("details");
-						$.set(elements[i]+"_1_details", {
-							x:full_width/2,
-							y:details_top,
-							width:full_width/2,
-							height:details_height,
-							borderRadius:"0px 25px 25px 0px"
-						});
-					}
-				}
-			}
 		};
 
-		function clicked(e){
-			console.log(e.currentTarget.id);
-			var group = e.currentTarget.id.slice(0,-2);
-			var side = parseInt(e.currentTarget.id.slice(-1));
-			var pair = side ? 0 : 1;
-
-			if(elements_data[group] == side){
-				console.log("back");
-				loadNew(elements.indexOf(group));
-			}else{
-				e.currentTarget.classList.add("active");
-				elements_data[group] = side;
-				var indx = elements.indexOf(group);
-				$.tween(e.currentTarget, 0.6, {
-					x:gap+(indx*small_setup.width)+(indx*gap),
-					y:gap,
-					width:small_setup.width,
-					height:small_setup.height,
-					borderRadius:"25px 25px 25px 25px",
-					ease:Power2.easeOut
-				});
-				$.tween(group, 0.6, {
-					x:0,
-					y:0,
-					width:small_setup.width,
-					ease:Power2.easeInOut
-				});
-				$.tween(group+"_"+pair, 0.4, {autoAlpha:0, ease:Power2.easeOut});
-				
-				if($.id(group+"_0_details")){
-					$.tween(group+"_0_details", 0.4, {autoAlpha:0, ease:Power2.easeOut});
-				}
-				if($.id(group+"_1_details")){
-					$.tween(group+"_1_details", 0.4, {autoAlpha:0, ease:Power2.easeOut});
-				}
-				loadNew(indx+1);
-			}
-		}
 		function loadNew(i){
-			var full_width = W-2*gap;
-			$.tween(elements[i], 0.8, {
-				autoAlpha:1,
-				x:gap,
-				y:passive_y,
-				width:full_width,
-				ease:Power1.easeInOut
-			});
-			if(elements[i] != "calc_input"){
-				$.id(elements[i]+"_0").classList.remove("active");
-				$.tween(elements[i]+"_0", 0.8, {
-					autoAlpha:1,
-					x:0,
-					y:0,
-					width:full_width/2,
-					height:element_height,
-					borderRadius:"25px 0px 0px 25px",
-					ease:Power2.easeInOut
-
-				});
-				if($.id(elements[i]+"_0_details")){
-					$.tween(elements[i]+"_0_details", 0.8, {autoAlpha:1, ease:Power2.easeInOut});
-				}
-				$.id(elements[i]+"_1").classList.remove("active");
-				$.tween(elements[i]+"_1", 0.8, {
-					autoAlpha:1,
-					x:full_width/2,
-					y:0,
-					width:full_width/2,
-					height:element_height,
-					borderRadius:"0px 25px 25px 0px",
-					ease:Power2.easeInOut
-				});
-				if($.id(elements[i]+"_1_details")){
-					$.tween(elements[i]+"_1_details", 0.8, {autoAlpha:1, ease:Power2.easeInOut});
-				}
-			}
-			for(var r = i; r < elements.length; r++){
-				elements_data[elements[r]] = null;
-				if(r != i){
-					$.tween(elements[r], 0.4, {autoAlpha:0});
-				}
-			}
+		
+			
 			if(i == 0){
 				$.tween("calc_title", 0.4, {autoAlpha:1, ease:Power2.easeOut});
 			}else{
 				$.tween("calc_title", 0.4, {autoAlpha:0, ease:Power2.easeOut});
 			}
 		};
-		function parser(){
-			var html = "";
-			var driver_num = parseInt($.id("calc_drivers_num").innerHTML);
-			var _data = data[elements_data["calc_main_buttons"] == 0 ? "workshop" : "licence"][elements_data["calc_sub_buttons"] == 0 ? "basic" : "pro"];
-			for(var i = 0; i < printer.length; i++){
-				html += "<div>"+printer[i].output +"<span>"+printer[i].calc(driver_num, ranges, _data)+"</span></div>";
+		function parser(val){
+			var max = 1000;
+			var s_val = getSectionValue(val, ranges, max);
+			var r = getRange(s_val, ranges);
+			$.id("calc_drivers_num").innerHTML = ranges[r][1]+"<br/>DRIVERS";
+			//$.id("calc_drivers_num").innerHTML = (val == max ? "5000+" : s_val)+"<br/>DRIVER"+(val != 1 ? "S" : "");
+
+			
+
+			for(key in data){
+				var cost = data[key][r];
+				$.id("saving_"+key).innerHTML = ranges[r][1]*50+70 - (cost*ranges[r][1]);
+				$.id("time_"+key).innerHTML = 4*ranges[r][1];
+				$.id("cost_"+key).innerHTML = '£'+cost;
+				$.id("salary_"+key).innerHTML = 70*ranges[r][1];
 			}
-			$.id("calc_cover_output").innerHTML = html;
-			$.tween("calc_cover", 0.8, {autoAlpha:1, ease:Power1.easeInOut});
-			console.log(html);
+
+			/*for(var i = 0; i < printer.length; i++){
+				html += "<div>"+printer[i].output +"<span>"+printer[i].calc(val, ranges, _data)+"</span></div>";
+			}*/
+			//$.id("calc_cover_output").innerHTML = html;
+			//$.tween("calc_cover", 0.8, {autoAlpha:1, ease:Power1.easeInOut});
+			//console.log(html);
 		}
 		function closeParser(){
 			$.tween("calc_cover", 0.6, {autoAlpha:0, ease:Power1.easeInOut});
@@ -316,40 +153,6 @@
 			var r = getSelection(n, _ranges, max);
 			var perc = (n-r*slice)/slice
 			return Math.round((_ranges[r][1] - _ranges[r][0])*perc+_ranges[r][0]);
-		}
-
-		function processPeople(n, _ranges, max){
-			$.set("calc_people", {x:-gap, y:-passive_y})
-			for(var i = people_length-1; i >= 0; i--){
-				$.create("p_"+i, "calc_people", {alpha:0, y:people_gap}, "img/img_"+i+".png");
-				people_state.push(false);
-			}
-			$.create("p_start", "calc_people", {}, "img/img_start.png");
-		}
-		function checkPeople(n, _ranges, max){
-			var r = n == 1 ? -1 : getSelection(n, _ranges, max);
-			
-			for(var i = 0; i < _ranges.length; i++){
-				if(i <= r && !people_state[i]){
-					people_state[i] = true;
-					animatePeople("in", i);
-				}
-				if(i > r && people_state[i]){
-					people_state[i] = false;
-					animatePeople("out", i);
-				}
-			}
-		}
-		function animatePeople(dir, i){
-			var tw = {};
-			var t = 0.8;
-			if(dir == "in"){
-				tw = {y:0, alpha:1, ease:Back.easeOut}
-			}else{
-				t = 0.5;
-				tw = {y:people_gap, alpha:0, ease:Power2.easeIn}
-			}
-			$.tween("p_"+i, t, tw);
 		}
 	}
 })({
